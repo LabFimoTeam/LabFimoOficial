7dda843c131ad865e48504246404d740e522cc1c
erdvillegas <vi.erik@yahoo.com>
2014-10-26 23:03:21 -0600

    Agregando el formulario

--- src/LabFimo/SecurityBundle/LabFimoSecurityBundle.php
+++ src/LabFimo/SecurityBundle/LabFimoSecurityBundle.php
@@ -4,6 +4,8 @@ namespace LabFimo\SecurityBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class LabFimoSecurityBundle extends Bundle
-{
+class LabFimoSecurityBundle extends Bundle {
+
+    
+
 }
